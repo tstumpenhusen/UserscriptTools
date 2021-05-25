debc11bd7a6d8341c2766696bd3ec4946739ec84
Tim Stumpenhusen <tim.stumpenhusen@gmx.de>
2021-05-25 21:43:56 +0200

    Added Advanced Keys

---
+++ AdvancedKeyEvents.js
@@ -0,0 +1,29 @@
+function keyMatches(modifierKeys, e) {
+	var comboParts = modifierKeys
+		.split("+")
+		.map(key => key.trim());
+	
+	comboParts = comboParts.map(part => part.toLowerCase());
+
+	if (comboParts.indexOf("ctrl") > 0 && !e.ctrlKey) {
+		return false;
+	}
+	
+	if (comboParts.indexOf("alt") > 0 && !e.altKey) {
+		return false;
+	}
+	
+	if (comboParts.indexOf("shift") > 0 && !e.shiftKey) {
+		return false;
+	}
+	
+	return true;
+}
+
+document.addAdvancedEventListener = function(modifiers, eventName, func) {
+	document.addEventListener(keyEvent, e => {
+		if (modifiersMatch(modifiers, e)) {
+			func(e);
+		}
+	});
+};
